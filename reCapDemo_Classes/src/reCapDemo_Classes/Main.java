ebeaac474327c3a80eca3a25f2443ac1f2df9342
burcu.bayik <burcu.bayik@BURCUBAYIK.etiyacorp.local>
2022-07-21 18:12:03 +0300

    classes added.

---
+++ reCapDemo_Classes/src/reCapDemo_Classes/Main.java
@@ -0,0 +1,12 @@
+package reCapDemo_Classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.topla(3, 4);
+		System.out.println(sonuc);
+
+	}
+
+}
