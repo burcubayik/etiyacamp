e6aed011a80a82eb0ef02af6f3e40f6da3bb25a3
burcu.bayik <burcu.bayik@BURCUBAYIK.etiyacorp.local>
2022-07-21 18:12:44 +0300

    classes added.

---
+++ classes/src/classes/CustomerManager.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class CustomerManager {
+	
+	public void add() {
+		System.out.println("müşteri eklendi.");
+	}
+	
+	public void remove() {
+		System.out.println("müşteri silindi");
+	}
+
+	public void update() {
+		System.out.println("müşteri güncellendi");
+	}
+}
---
+++ classes/src/classes/Main.java
@@ -0,0 +1,34 @@
+package classes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+		CustomerManager customerManager=new CustomerManager();
+		CustomerManager customerManager2=new CustomerManager();
+		customerManager=customerManager2;
+		customerManager.add();
+		customerManager.remove();
+		customerManager.update();
+		
+	
+		
+		int sayi1=10;
+		int sayi2=20;
+		sayi2=sayi1;
+		sayi1=30;
+		System.out.println(sayi2);
+		
+		
+		int[] sayilar1=new int[] {1,2,3};
+		int[] sayilar2=new int[] {4,5,6};
+		sayilar1=sayilar2;
+		sayilar1[0]=10;
+		System.out.println(sayilar2[0]); //10
+		
+	}
+
+}
+
+
