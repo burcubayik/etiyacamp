0e1585db91e34a3bd590671792598f43a50b4f27
burcu.bayik <burcu.bayik@BURCUBAYIK.etiyacorp.local>
2022-07-21 18:09:13 +0300

    classlarlaCalismak added

---
+++ classlarlaCalismak/src/classlarlaCalismak/CustomerManager.java
@@ -0,0 +1,20 @@
+package classlarlaCalismak;
+
+public class CustomerManager {
+
+	public void add() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void remove() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ classlarlaCalismak/src/classlarlaCalismak/Main.java
@@ -0,0 +1,15 @@
+package classlarlaCalismak;
+
+public class Main {
+
+	public static void main(String[] args) {
+		CustomerManager customerManager = new CustomerManager();
+		customerManager.add();
+		customerManager.remove();
+		customerManager.update();
+		
+		
+
+	}
+
+}
